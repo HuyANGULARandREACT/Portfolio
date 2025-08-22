64b4918e7f37b89f27657ee1c04b55aef69c8860
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2025-08-22 12:19:08 +0700

    push

--- json-api-vercel/index.js
+++ json-api-vercel/index.js
@@ -1,10 +1,14 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+const PORT = process.env.PORT || 3000;
 
-// Export function cho Vercel
-module.exports = server
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
