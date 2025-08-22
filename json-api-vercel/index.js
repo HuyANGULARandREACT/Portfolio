b691ef60e5430760c0edd6c71b703cfefa03e816
nguyen-gia-huy <nguyenhuy3112005@gmail.com>
2025-08-22 12:00:36 +0700

    push 3

--- json-api-vercel/index.js
+++ json-api-vercel/index.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(8080, () => {
-  console.log('JSON Server is running')
-})
+
+// Export function cho Vercel
+module.exports = server
